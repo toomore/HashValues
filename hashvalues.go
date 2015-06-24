829dec7b60276e766c2b0a298f70174b5fa4e8ee
Toomore Chiang <toomore0929@gmail.com>
2015-06-24 16:09:15 +0800

    Add Get, Set, Del, Add.

--- hashvalues.go
+++ hashvalues.go
@@ -18,3 +18,19 @@ func NewHashValues(hashkey []byte, hashfunc hash.Hash) *HashValues {
 		hashkey:  hashkey,
 	}
 }
+
+func (h *HashValues) Set(key, value string) {
+	h.Values.Set(key, value)
+}
+
+func (h *HashValues) Add(key, value string) {
+	h.Values.Add(key, value)
+}
+
+func (h *HashValues) Del(key string) {
+	h.Values.Del(key)
+}
+
+func (h *HashValues) Get(key string) string {
+	return h.Values.Get(key)
+}
